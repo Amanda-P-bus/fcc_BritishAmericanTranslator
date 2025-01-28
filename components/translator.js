2ceeb2a46628022d4e794d5c6b97feb369f143f2
Amanda Pollard <AmandaPhere@gmail.com>
2025-01-28 16:14:36 -0500

    feat: noNeed working to accept or reject potential translations while filtering out partial matches. moving on to translating

--- components/translator.js
+++ components/translator.js
@@ -2,8 +2,114 @@ const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
+const allWords = require('./allWords.js')
+
+//
 
 class Translator {
+noNeed(text, locale) {
+
+//amerian objects
+const americanOnlyObj = Object.keys(americanOnly);
+const americanSpellingObj = Object.keys(americanToBritishSpelling);
+const americanTitlesObj = Object.keys(americanToBritishTitles);
+
+//british objects
+const britishOnlyObj = Object.keys(britishOnly);
+const britishSpellingObj = Object.values(americanToBritishSpelling);
+const britishTitlesObj = Object.values(americanToBritishTitles);
+
+
+if (locale === 'american-to-british') {
+
+
+
+//arr of all possible american words to translate
+const allAmerican = americanOnlyObj.concat(americanSpellingObj, americanTitlesObj);
+
+let textArr = text.split(" ")
+
+//below checks for all matching words!
+    //hightlight and translate these
+let checkAmericanArr = Array.from(allAmerican).filter(word => textArr.includes(word));
+
+//returns words that DO NOT match British only words
+    //do not highlight or translate these
+let cleanArray = textArr.filter(element => checkAmericanArr.every(item => !element.includes(item)));
+
+
+console.log(checkAmericanArr);
+console.log(cleanArray);}
+
+/*
+    if (americanOnlyIncludes === true)
+    {console.log("On American Only sheet")}
+    
+   else if (americanSpellingIncludes === true)
+    {console.log("On American Spelling sheet")}
+
+   else if (americanTitlesIncludes === true) 
+        {console.log("On American Titles sheet")}
+
+    else { console.log("not american") }
+}
+*/
+
+if (locale === 'british-to-american') {
+
+
+//arr of all possible american words to translate
+const allBritish = britishOnlyObj.concat(britishSpellingObj, britishTitlesObj);
+
+let textArr = text.split(" ")
+
+//below checks for all matching words!
+    //hightlight and translate these
+let checkBritishArr = Array.from(allBritish).filter(word => textArr.includes(word));
+
+//returns words that DO NOT match British only words
+    //do not highlight or translate these
+let cleanArray = textArr.filter(element => checkBritishArr.every(item => !element.includes(item)));
+
+
+console.log(checkBritishArr, checkBritishArr.length);
+console.log(cleanArray, cleanArray.length);
+
+    if (checkBritishArr.length === 0) 
+        {return "No words found to translate"}
+
+    else {return cleanArray}
+}
+ /*
+    if (britishOnlyIncludes === true)
+    {console.log("On British Only sheet")}
+
+    else if (britishSpellingIncludes === true)
+    {console.log("On British Spelling sheet")}
+
+   else if (britishTitlesIncludes === true)
+        {console.log("On British Titles sheet")}
+
+   else { console.log("not british") }
+}
+*/
+//else { console.log("not american or british") }
+ 
+//if locale = american-to-british    
+ //for loop to create arr of just american and just british words? 
+
+//loop through here?
+}
+
+shouldHighlight(text, _cleanArray) {
+
+console.log(this.cleanArray + "ugh")
+//make text array, split it, then loop through to check each imported list?
+}
+americanToBritish(text) {}
+
+britishToAmerican(text) {}
+
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
+const allWords = require('../components/allWords.js');
 
 module.exports = function (app) {
   
@@ -8,6 +9,52 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
-    });
+      const { text } = req.body;
+      const { locale } = req.body;
+      const americanOnlyArr = allWords.americanOnlyArr;
+      const britishOnlyArr = allWords.britishOnlyArr;
+      const americanToBritishSpellingArr = allWords.americanToBritishSpellingArr;
+      const americanToBritishTitlesArr = allWords.americanToBritishTitlesArr;
+
+        
+      if (!locale)
+      {return res.json({ error: 'Required field(s) missing' })}
+
+      if (!text)
+        {return res.json({ error: 'No text to translate' })}
+
+      if (locale !== "american-to-british" && locale !== "british-to-american")
+        {return res.json({ error: 'Invalid value for locale field' })}
+
+      //If text requires no translation, return "Everything looks good to me!"
+
+      const shouldTranslate = translator.noNeed(text, locale)
+
+      if (shouldTranslate === true)
+      {console.log("move to highlighting")
+        translator.shouldHighlight(text)
+      }
+
+      if (shouldTranslate === false)
+      {res.json("Everything looks good to me!")}
+
+
+    }); //end post request
 };
+
+/*
+2. You can POST to /api/translate with a body containing text with the text to translate and locale with either american-to-british or british-to-american. The returned object should contain the submitted text and translation with the translated text.
+
+3. The /api/translate route should handle the way time is written in American and British English. For example, ten thirty is written as "10.30" in British English and "10:30" in American English. The span element should wrap the entire time string, i.e. <span class="highlight">10:30</span>.
+
+4. The /api/translate route should also handle the way titles/honorifics are abbreviated in American and British English. For example, Doctor Wright is abbreviated as "Dr Wright" in British English and "Dr. Wright" in American English. See /components/american-to-british-titles.js for the different titles your application should handle.
+
+5. Wrap any translated spelling or terms with <span class="highlight">...</span> tags so they appear in green.
+6. If one or more of the required fields is missing, return { error: 'Required field(s) missing' }.
+
+7. If text is empty, return { error: 'No text to translate' }
+
+8. If locale does not match one of the two specified locales, return { error: 'Invalid value for locale field' }.
+
+9. If text requires no translation, return "Everything looks good to me!" for the translation value.
+*/
