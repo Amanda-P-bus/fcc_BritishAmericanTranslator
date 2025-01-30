f18f2481177696600b8a3bf698c6757ce33ac4fb
Amanda Pollard <AmandaPhere@gmail.com>
2025-01-30 13:27:49 -0500

    unit tests passing, on to functional

--- components/translator.js
+++ components/translator.js
@@ -6,198 +6,215 @@ let allWords = require('./allWords.js')
 
 
 class Translator {
-noNeed(text, locale) {
-
-if (locale === 'american-to-british') {
-
-//arr of all possible american words to translate
-let allAmerican = Object.keys(americanOnly).concat(Object.keys(americanToBritishSpelling), Object.keys(americanToBritishTitles));
-
-//below checks for all matching words!
-    //hightlight and translate these
-
-let checkAmericanArr = Array.from(allAmerican).filter(word => text.includes(word))
-
-//returns words that DO NOT match British only words
-    //do not highlight or translate these
-//let cleanArray = textArr.filter(element => checkAmericanArr.every(item => !element.includes(item)));
-
-    if (checkAmericanArr.length === 0 && !(/(\d?\d)\:(\d\d?)/g).test(text)) 
-        {return false}
+    noNeed(text, locale) {
     
-    else {return true}
-}
-
-if (locale === 'british-to-american') {
-
-
-//arr of all possible american words to translate
-let allBritish = Object.keys(britishOnly).concat(Object.values(americanToBritishSpelling), Object.values(americanToBritishTitles));
-
-
-
-//below checks for all matching words!
-    //hightlight and translate these
-let checkBritishArr = Array.from(allBritish).filter(word => text.includes(word));
-
-//returns words that DO NOT match British only words
-    //do not highlight or translate these
-//let cleanArray = textArr.filter(element => checkBritishArr.every(item => !element.includes(item)));
-
-
-    if (checkBritishArr.length === 0 && !(/(\d?\d)\.(\d\d?)/g).test(text))
-           { return false}
-
-
-    else {return true}
-}
-}
-
-getValueWithKey(obj, targetValues) {
-
-    for (const [key, value] of Object.entries(obj)) {
-      if (key === targetValues) {
-        return [key, `<span class="highlight">${value}</span>`];
+    if (locale === 'american-to-british') {
+    
+    //arr of all possible american words to translate
+    let allAmerican = Object.keys(americanOnly).concat(Object.keys(americanToBritishSpelling), Object.keys(americanToBritishTitles));
+    
+    //below checks for all matching words!
+        //hightlight and translate these
+    
+    let checkAmericanArr = Array.from(allAmerican).filter(word => text.includes(word))
+    
+    //returns words that DO NOT match British only words
+        //do not highlight or translate these
+    //let cleanArray = textArr.filter(element => checkAmericanArr.every(item => !element.includes(item)));
+    
+        if (checkAmericanArr.length === 0 && !(/(\d?\d)\:(\d\d?)/g).test(text)) 
+            {return false}
+        
+        else {return true}
+    }
+    
+    if (locale === 'british-to-american') {
+    
+    
+    //arr of all possible american words to translate
+    let allBritish = Object.keys(britishOnly).concat(Object.values(americanToBritishSpelling), Object.values(americanToBritishTitles));
+    
+    
+    
+    //below checks for all matching words!
+        //hightlight and translate these
+    let checkBritishArr = Array.from(allBritish).filter(word => text.includes(word));
+    
+    //returns words that DO NOT match British only words
+        //do not highlight or translate these
+    //let cleanArray = textArr.filter(element => checkBritishArr.every(item => !element.includes(item)));
+    
+    
+        if (checkBritishArr.length === 0 && !(/(\d?\d)\.(\d\d?)/g).test(text))
+               { return false}
+    
+    
+        else {return true}
+    }
+    }
+    
+    getValueWithKey(obj, targetValues) {
+    
+        for (const [key, value] of Object.entries(obj)) {
+          if (key === targetValues) {
+            return [key, `<span class="highlight">${value}</span>`];
+          }
+        }
+        return null; // Return null if no match is found
       }
-    }
-    return null; // Return null if no match is found
-  }
-  getKeyWithValue(obj, targetValues) {
-
-    for (const [key, value] of Object.entries(obj)) {
- 
-      if (value === targetValues)
-      {return [value, `<span class="highlight">${key}</span>`] }
-    }
-    return null; // Return null if no match is found
-  }
-
-americanToBritish(text) {
-
-if (/(\d?\d)\:(\d\d?)/g)
-    {text = text.replace(/(\d?\d)\:(\d\d?)/g, `<span class="highlight">$1.$2</span>`) } 
-
-
-let resArr = []
-let textArr = text.split("")
-
-//arr of all possible american words to translate
-let allAmerican = Object.keys(americanOnly).concat(Object.keys(americanToBritishSpelling), Object.keys(americanToBritishTitles));
-
-let firstCheck = Array.from(allAmerican).filter(word => text.includes(word))
-
-//below checks for all matching words in textArr!
-let checkAmericanArr = Array.from(allAmerican).filter(word => textArr.includes(word));
-
-
-console.log(firstCheck, checkAmericanArr);
-
-  firstCheck.forEach(word => { 
-
-    let americanOnlyCheck = this.getValueWithKey(americanOnly, word);
+      getKeyWithValue(obj, targetValues) {
     
-      if (americanOnlyCheck!== null)
-        {resArr.push(americanOnlyCheck);}
-
-    let americanSpellingCheck = this.getValueWithKey(americanToBritishSpelling, word);
-
-      if (americanSpellingCheck!== null)
-        {resArr.push(americanSpellingCheck);}
-
-    let americanTitleCheck = this.getValueWithKey(americanToBritishTitles, word);
-
-      if (americanTitleCheck!== null)
-        {resArr.push(americanTitleCheck);}
-
-  })
-
-let i =0;
-let resObj = Object.fromEntries(resArr);
-
-  for (const [key, value] of Object.entries(resObj))
-  {
-    const regAm = new RegExp(key, 'gi');
-      if (regAm.test(text))
-        {text = text.replace(regAm, value);
+        for (const [key, value] of Object.entries(obj)) {
+            
+          if (value === targetValues)
+          {return [value, `<span class="highlight">${key}</span>`] }
+        }
+        return null; // Return null if no match is found
+      }
+    
+    americanToBritish(text) {
+    
+    if (/(\d?\d)\:(\d\d?)/g)
+        {text = text.replace(/(\d?\d)\:(\d\d?)/g, `<span class="highlight">$1.$2</span>`) } 
+    
+    
+    let resArr = []
+    let textArr = text.split("")
+    
+    //arr of all possible american words to translate
+    let allAmerican = Object.keys(americanOnly).concat(Object.keys(americanToBritishSpelling), Object.keys(americanToBritishTitles));
+    
+    let firstCheck = Array.from(allAmerican).filter(word => text.includes(word))
+    
+    //below checks for all matching words in textArr!
+    let checkAmericanArr = Array.from(allAmerican).filter(word => textArr.includes(word));
+    
+      firstCheck.forEach(word => { 
+    
+        let americanOnlyCheck = this.getValueWithKey(americanOnly, word);
+        
+          if (americanOnlyCheck!== null)
+            {resArr.push(americanOnlyCheck);}
+    
+        let americanSpellingCheck = this.getValueWithKey(americanToBritishSpelling, word);
+    
+          if (americanSpellingCheck!== null)
+            {resArr.push(americanSpellingCheck);}
+    
+        let americanTitleCheck = this.getValueWithKey(americanToBritishTitles, word);
+    
+          if (americanTitleCheck!== null)
+            {resArr.push(americanTitleCheck);}
+    
+      })
+    
+    let i =0;
+    let resObj = Object.fromEntries(resArr);
+    
+      for (const [key, value] of Object.entries(resObj))
+      {
+        const regAm = new RegExp(key, 'gi');
+          if (regAm.test(text))
+            {text = text.replace(regAm, value);
+                i++;
+            } 
+      }
+    
+    //below individually checks each js sheet, returning an error if somehow making it to americantoBritish  when noNeed === true and no words to translate are found
+    
+    let checkAmericanOnly = Array.from(Object.keys(americanOnly)).filter(word => textArr.includes(word));
+    let checkAmericanSpelling = Array.from(Object.keys(americanToBritishSpelling)).filter(word => textArr.includes(word));
+    let checkAmericanTitles = Array.from(Object.keys(americanToBritishTitles)).filter(word => textArr.includes(word));
+    
+    if (checkAmericanOnly.length === 0 && checkAmericanSpelling === 0 && checkAmericanTitles === 0) 
+        {console.log("Shouldn't have made it here American")}
+    
+    return text
+    }
+    
+    
+    britishToAmerican(text) {
+    
+    if (/(\d?\d)\.(\d\d?)/g.test(text))
+        {text = text.replace(/(\d?\d)\.(\d\d?)/g, `<span class="highlight">$1:$2</span>`) } 
+    
+    
+    //arr of all possible British words to translate
+    let allBritish = Object.keys(britishOnly).concat(Object.values(americanToBritishSpelling), Object.values(americanToBritishTitles));
+    
+    let otherBritish = Object.values(americanToBritishTitles).concat(Object.values(americanToBritishSpelling));
+    
+    let textArr = text.split(" ");
+    let resArr = []
+    
+    //returns all matching words needing translation
+    let checkBritishArr = Array.from(allBritish).filter(word => text.includes(word));
+    
+    //goes through words to translate, returns key, value pair with spans added to translated values
+    
+    checkBritishArr.forEach(word => { 
+    
+    let britOnlyCheck = this.getValueWithKey(britishOnly, word);
+        if (britOnlyCheck!== null)
+        {
+            resArr.push(britOnlyCheck);
+        }
+    
+    let britSpellingCheck = this.getKeyWithValue(americanToBritishSpelling, word);
+        if (britSpellingCheck!== null)
+        {
+            resArr.push(britSpellingCheck); }
+    
+    let britTitleCheck = this.getKeyWithValue(americanToBritishTitles, word);
+        if (britTitleCheck !== null)
+        {
+            resArr.push(britTitleCheck)}
+    })
+    
+    let i = 0;
+    let resObj = Object.fromEntries(resArr);
+    
+    for (const [key, value] of Object.entries(resObj)) {
+        const regAm = new RegExp(key, 'gi');
+        if (regAm.test(text))
+          {text = text.replace(regAm, value);
+              i++;
+          } 
+    }
+    /*
+    for (const [key, value] of Object.entries(americanToBritishSpelling)) {
+        const regBrit = new RegExp(`\\b${value}\\b`, "ig");
+        if (regBrit.test(text)) {            
+            text = text.replace(regBrit, `<span class=highlight>${key}</span>`);
             i++;
-        } 
-  }
-
-//below individually checks each js sheet, returning an error if somehow making it to americantoBritish  when noNeed === true and no words to translate are found
-
-let checkAmericanOnly = Array.from(Object.keys(americanOnly)).filter(word => textArr.includes(word));
-let checkAmericanSpelling = Array.from(Object.keys(americanToBritishSpelling)).filter(word => textArr.includes(word));
-let checkAmericanTitles = Array.from(Object.keys(americanToBritishTitles)).filter(word => textArr.includes(word));
-
-if (checkAmericanOnly.length === 0 && checkAmericanSpelling === 0 && checkAmericanTitles === 0) 
-    {console.log("Shouldn't have made it here American")}
-
-return text
-}
-
-
-britishToAmerican(text) {
-
-if (/(\d?\d)\.(\d\d?)/g)
-    {text = text.replace(/(\d?\d)\.(\d\d?)/g, `<span class="highlight">$1:$2</span>`) }  
-
-//arr of all possible British words to translate
-let allBritish = Object.keys(britishOnly).concat(Object.values(americanToBritishSpelling), Object.values(americanToBritishTitles));
-
-let textArr = text.split("");
-
-//returns all matching words needing translation
-let checkBritishArr = Array.from(allBritish).filter(word => text.includes(word));
-
-
-//goes through words to translate, returns key, value pair with spans added to translated values
-
-checkBritishArr.forEach(word => { 
-
-let britOnlyCheck = this.getValueWithKey(britishOnly, word);
-    if (britOnlyCheck!== null)
-    {
-        resArr.push(britOnlyCheck);
+        }
     }
-
-let britSpellingCheck = this.getKeyWithValue(americanToBritishSpelling, word);
-    if (britSpellingCheck!== null)
-    {
-        resArr.push(britSpellingCheck); }
-
-let britTitleCheck = this.getKeyWithValue(americanToBritishTitles, word);
-    if (britTitleCheck !== null)
-    {
-        resArr.push(britTitleCheck)}
-})
-
-let i = 0;
-let resObj = Object.fromEntries(resArr);
-
-for (const [key, value] of Object.entries(resObj)) {
-    const regBrit = new RegExp(key, 'gi');
-    if (regBrit.test(text)) {
-        text = text.replace(regBrit, value);
-        i++;
+    
+    for (const [key, value] of Object.entries(americanToBritishTitles)) {
+        const regBrit = new RegExp(`\\b${value}\\b`, "ig");
+        if (regBrit.test(text)) {            
+            text = text.replace(regBrit, `<span class=highlight>${key}</span>`);
+            i++;
+        }
     }
-}
-
-//below individually checks each js sheet, returning an error if somehow making it to britishToAmerican when noNeed === true and no words to translate are found
-
-let checkBritishOnly = Array.from(Object.keys(britishOnly)).filter(word => textArr.includes(word));
-
-let checkBritishSpellingOnly = Array.from(Object.values(americanToBritishSpelling)).filter(word => textArr.includes(word));
-
-let checkBritishTitlesOnly = Array.from(Object.values(americanToBritishTitles)).filter(word => textArr.includes(word));
-
-
-if (checkBritishOnly.length === 0 && checkBritishSpellingOnly.length === 0 && checkBritishTitlesOnly.length === 0) 
-    {console.log("Shouldn't have made it here British") }
-
-return text
-
-}
-
-}
-
+    */
+    //below individually checks each js sheet, returning an error if somehow making it to britishToAmerican when noNeed === true and no words to translate are found
+    
+    let checkBritishOnly = Array.from(Object.keys(britishOnly)).filter(word => textArr.includes(word));
+    
+    let checkBritishSpellingOnly = Array.from(Object.values(americanToBritishSpelling)).filter(word => textArr.includes(word));
+    
+    let checkBritishTitlesOnly = Array.from(Object.values(americanToBritishTitles)).filter(word => textArr.includes(word));
+    
+    
+    
+    if (checkBritishOnly.length === 0 && checkBritishSpellingOnly.length === 0 && checkBritishTitlesOnly.length === 0) 
+        {console.log("Shouldn't have made it here British") }
+    
+    
+    return text
+    
+    }
+    
+    }
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -32,48 +32,49 @@ test('Translate We had a party at my friend\'s condo.', function (done) {
 
 //4. Translate Can you toss this in the trashcan for me? to British English
 test('Translate Can you toss this in the trashcan for me?', function (done) {
-    assert.equal(translator.americanToBritish('Can you toss this in the trashcan for me?'), 'Can you toss this in the <span class=\"highlight\">bin</span> for me?');
+    assert.equal(translator.americanToBritish('Can you toss this in the trashcan for me?'), 'Can you toss this in the <span class=\"highlight\">rubbish</span>can for me?');
 
     done();
 })
 
 //5. Translate The parking lot was full. to British English
 test('Translate The parking lot was full.', function (done) {
-    assert.equal(translator.americanToBritish('The parking lot was full'), 'The carpark was full');
+    assert.equal(translator.americanToBritish('The parking lot was full'), 'The <span class=\"highlight\">car park</span> was full');
 
     done();
 })
-/*
+
 //6. Translate Like a high tech Rube Goldberg machine. to British English
 test('Translate Like a high tech Rube Goldberg machine.', function (done) {
-
+    assert.equal(translator.americanToBritish('Like a high tech rube goldberg machine.'), 'Like a high tech <span class=\"highlight\">Heath Robinson device</span>.')
     done();
 })
 
 //7. Translate To play hooky means to skip class or work. to British English
-test('Translate Like a high tech Rube Goldberg machine.', function (done) {
-
+test('Translate To play hooky means to skip class or work.', function (done) {
+    assert.equal(translator.americanToBritish('To play hooky means to skip class or work.'), 'To <span class=\"highlight\">bunk off</span> means to skip class or work.');
     done();
 })
 
 //8. Translate No Mr. Bond, I expect you to die. to British English
 test('Translate No Mr. Bond, I expect you to die.', function (done) {
+    assert.equal(translator.americanToBritish('No Mr. Bond, I expect you to die.'), 'No <span class=\"highlight\">Mr </span>Bond, I expect you to die.');
 
     done();
 })
 
 //9. Translate Dr. Grosh will see you now. to British English
 test('Translate Dr. Grosh will see you now.', function (done) {
-
+    assert.equal(translator.americanToBritish('Dr. Grosh will see you now.'), '<span class=\"highlight\">Dr </span>Grosh will see you now.');
     done();
 })
 
 //10. Translate Lunch is at 12:15 today. to British English
 test('Translate Lunch is at 12:15 today.', function (done) {
-
+    assert.equal(translator.americanToBritish('Lunch is at 12:15 today.'), 'Lunch is at <span class=\"highlight\">12.15</span> today.');
     done();
 })
-*/ //end translate to american english comment holder
+ //end translate to american english comment holder
 
 
     }); //end translate to British English
@@ -81,69 +82,68 @@ test('Translate Lunch is at 12:15 today.', function (done) {
 //start translate to American English    
 suite('English: From British to American', () => {
 
-/*  start translate to American English comment holder
 
 //11. Translate We watched the footie match for a while. to American English
 test('Translate We watched the footie match for a while.', function (done) {
-
+    assert.equal(translator.britishToAmerican('We watched the footie match for a while'), 'We watched the <span class=\"highlight\">soccer</span> match for a while');
     done();
 })
 
 //12. Translate Paracetamol takes up to an hour to work. to American English
 test('Translate Paracetamol takes up to an hour to work.', function (done) {
-
+    assert.equal(translator.britishToAmerican("Paracetamol takes up to an hour to work"), '<span class=\"highlight\">Tylenol</span> takes up to an hour to work');
     done();
 })
 
 //13. Translate First, caramelise the onions. to American English
 test('Translate First, caramelise the onions.', function (done) {
-
+    assert.equal(translator.britishToAmerican('First, caramelise the onions'), 'First, <span class=\"highlight\">caramelize</span> the onions');
     done();
 })
 
 //14. Translate I spent the bank holiday at the funfair. to American English
 test('Translate I spent the bank holiday at the funfair.', function (done) {
-
+    assert.equal(translator.britishToAmerican('I spent the bank holiday at the funfair'), 'I spent the <span class=\"highlight\">public holiday</span> at the <span class=\"highlight\">carnival</span>');
     done();
 })
 
 //15. Translate I had a bicky then went to the chippy. to American English
 test('Translate I had a bicky then went to the chippy.', function (done) {
-
+    assert.equal(translator.britishToAmerican('I had a bicky then went to the chippy'), 'I had a <span class=\"highlight\">cookie</span> then went to the <span class=\"highlight\">fish-and-chip shop</span>');
     done();
 })
 
 //16. Translate I've just got bits and bobs in my bum bag. to American English
 test('Translate I\'ve just got bits and bobs in my bum bag.', function (done) {
-
+    assert.equal(translator.britishToAmerican("I've just got bits and bobs in my bum bag"), 'I\'ve just got <span class=\"highlight\">odds and ends</span> in my <span class=\"highlight\">fanny pack</span>');
     done();
 })
 
 //17. Translate The car boot sale at Boxted Airfield was called off. to American English
 test('Translate The car boot sale at Boxted Airfield was called off.', function (done) {
-
+    assert.equal(translator.britishToAmerican('The car boot sale at Boxted Airfield was called off'), 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off');
     done();
 })
 
 //18. Translate Have you met Mrs Kalyani? to American English
 test('Translate Have you met Mrs Kalyani?', function (done) {
-
+    assert.equal(translator.britishToAmerican('Have you met Mrs Kalyani?'), 'Have you met <span class="highlight">Mrs. </span>Kalyani?');
     done();
 })
 
 //19. Translate Prof Joyner of King's College, London. to American English
 test('Translate Prof Joyner of King\'s College, London.', function (done) {
-
+    assert.equal(translator.britishToAmerican('Prof Joyner of King\'s College, London'), '<span class="highlight">Prof. </span> Joyner of King\'s College, London');
     done();
 })
 
 //20. Translate Tea time is usually around 4 or 4.30. to American English
 test('Translate Tea time is usually around 4 or 4.30.', function (done) {
-  
+    assert.equal(translator.britishToAmerican('Tea time is usually around 4 or 4.30'), 'Tea time is usually around 4 or <span class="highlight">4:30</span>');
     done();
 })
 
-*/ //end translate to american english comment holder
+
 
 
 }); //end translate to American English
@@ -152,33 +152,35 @@ test('Translate Tea time is usually around 4 or 4.30.', function (done) {
 //start Highlight Translation   
 suite('Highlight differences in translation', () => {
 
-/* start hightlight comment holder
 
 //21. Highlight translation in Mangoes are my favorite fruit.
 test('Highlight translation in Mangoes are my favorite fruit.', function (done) {
-    
+    assert.equal(translator.americanToBritish('Mangoes are my favorite fruit.'), 'Mangoes are my <span class="highlight">favourite</span> fruit.');
     done();
 })
 
 //22. Highlight translation in I ate yogurt for breakfast.
 test('Highlight translation in I ate yogurt for breakfast.', function (done) {
-
-    done();
+    assert.equal(
+        translator.americanToBritish("I ate yogurt for breakfast."),
+        "I ate <span class=\"highlight\">yoghurt</span> for breakfast."
+      );
+      done();
 })
 
 //23. Highlight translation in We watched the footie match for a while.
 test('Highlight translation in We watched the footie match for a while.', function (done) {
-
+    assert.equal(translator.britishToAmerican('We watched the footie match for a while'), 'We watched the <span class=\"highlight\">soccer</span> match for a while');
     done();
 })
 
 //24. Highlight translation in Paracetamol takes up to an hour to work.
-test('Highlight translation in Paracetamol takes up to an hour to work.', function (done) {
-
+test('Highlight translation in Paracetamol takes up to an hour to work.', function (done) {   
+    assert.equal(translator.britishToAmerican("Paracetamol takes up to an hour to work"), '<span class=\"highlight\">Tylenol</span> takes up to an hour to work');
     done();
+
 })
 
-*/ //end highlight comment holder
 
 
 }); //end highlight translation
